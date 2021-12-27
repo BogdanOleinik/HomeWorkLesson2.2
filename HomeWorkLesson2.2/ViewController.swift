9910a8fc350cdfb7ec1118eacb87313cec0ff812
Bogdan Oleinik <bogdan.swedbe@yandex.ru>
2021-12-27 19:37:39 +0300

    Initial Commit

---
+++ HomeWorkLesson2.2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HomeWorkLesson2.2
+//
+//  Created by Богдан Олейник on 27.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var viewFrameColor: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewFrameColor.layer.cornerRadius = 10
+        
+    }
+    
+
+
+}
+
